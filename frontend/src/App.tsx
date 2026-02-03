0fcd5fad14a893a2d439d99802160125a71c3bdb
Lenovictor <lenovictor145@gmail.com>
2026-02-03 18:58:32 +0100

    Update API_URL to point to tasks endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "https://task-manager-api2.onrender.com";
+const API_URL = "https://task-manager-api2.onrender.com/tasks";
 
 interface Task {
   id: number;
